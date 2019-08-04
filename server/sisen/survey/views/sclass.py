fa709aedcfd83fa87ed567aa566b29c9b3f3959c
Bruno G M Correa <brunogmc@gmail.com>
2019-08-04 19:21:31 +0100

    Adds order_by clause to Classes query

--- server/sisen/survey/views/sclass.py
+++ server/sisen/survey/views/sclass.py
@@ -13,7 +13,7 @@ def list(request, institution_id, program_id, format=None):
         'A instituição solicitada não existe (ID=%i)' % institution_id)
     try:
         program = institution.programs.get(pk=program_id)
-        return Response(ClassSerializer(program.classes, many=True).data)
+        return Response(ClassSerializer(program.classes.order_by('description'), many=True).data)
     except models.Program.DoesNotExist:
         raise NotFound(
             ('O programa solicitado (ID=%i) não existe ou não está vinculado'
